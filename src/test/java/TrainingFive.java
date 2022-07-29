f827114002193462ba797ec9958dcd6d4b4a5a16
Ilona27 <ilona.kohan2003@gmail.com>
2022-07-29 17:33:12 +0300

    commit3

--- src/test/java/TrainingFive.java
+++ src/test/java/TrainingFive.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class TrainingFive {
+import org.testng.annotations.Test;
+
+public class TrainingFive {
+    @Test
+    public void Stars(){
+        for(int i = 0; i < 3; i++){
+            for(int n = 0; n < 5; n++) {
+                System.out.print("* ");
+            }
+            System.out.println("* ");
+        }
+    }
 }
--- src/test/java/TrainingFour.java
+++ src/test/java/TrainingFour.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class TrainingFour {
+import org.testng.annotations.Test;
+
+public class TrainingFour {
+@Test
+    public void TestNums(){
+        int i = 1;
+        while (i <= 512){
+            System.out.println(i);
+            i *=2;
+        }
+    }
+
 }
